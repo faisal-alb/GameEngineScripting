0c7e93674cd72dfa8bfc922818354a927ecc4f87
Faisal <98351024+faisal-alb@users.noreply.github.com>
2024-02-24 20:01:52 -0600

    Hanoi Tower Final

--- Assets/Week3/Scripts/NewHanoiTower.cs
+++ Assets/Week3/Scripts/NewHanoiTower.cs
@@ -16,8 +16,16 @@ public class NewHanoiTower : MonoBehaviour
     [SerializeField] private int[] peg2 = { 0, 0, 0, 0 };
     [SerializeField] private int[] peg3 = { 0, 0, 0, 0 };
 
+    [SerializeField] private GameObject winInfo;
+
     private GameObject pointer;
+    private Transform currentDisc;
+    
+    private int[] fromArray;
+    private int fromIndex;
     private int currentPeg = 1;
+    
+    private bool isDiscUp = false;
 
     private void Start()
     {
@@ -40,18 +48,94 @@ public class NewHanoiTower : MonoBehaviour
                 disc.transform.localPosition = new Vector3(0, newPosition, 0);
             }
         }
+        
+        winInfo.gameObject.SetActive(false);
     }
 
     public void MoveRight()
     {
         IncrementPeg();
         UpdatePointer();
+
+        if (isDiscUp)
+        {
+            Transform currentPegTransform = GetPegTransform(currentPeg);
+        
+            currentDisc.SetParent(currentPegTransform);
+            currentDisc.localPosition = new Vector3(0, 480.0f, 0);
+        }
     }
 
     public void MoveLeft()
     {
         DecrementPeg();
         UpdatePointer();
+        
+        if (isDiscUp)
+        {
+            Transform currentPegTransform = GetPegTransform(currentPeg);
+        
+            currentDisc.SetParent(currentPegTransform);
+            currentDisc.localPosition = new Vector3(0, 480.0f, 0);
+        }
+    }
+
+    public void MoveUp()
+    {
+        if (isDiscUp) return;
+        
+        fromArray = GetPeg(currentPeg);
+        fromIndex = GetTopNumberIndex(fromArray);
+        
+        currentDisc = PopDiscFromCurrentPeg();
+
+        currentDisc.transform.localPosition = new Vector3(0, 480.0f, 0);
+
+        isDiscUp = true;
+    }
+
+    // Minor Bug, can't move disc back down to original peg because it's still in the same place in the array.
+    public void MoveDown()
+    {
+        if (currentDisc != null)
+        {
+            int[] toArray = GetPeg(currentPeg);
+            int toIndex = GetIndexOfFreeSlot(toArray);
+
+            if (toIndex == -1) return;
+
+            if (!CanAddToPeg(fromArray[fromIndex], toArray)) return;
+            
+            Transform currentPegTransform = GetPegTransform(currentPeg);
+
+            float newPosition = (toArray.Length - toIndex - 1) * (40.0f + 1.0f);
+
+            currentDisc.SetParent(currentPegTransform);
+            currentDisc.localPosition = new Vector3(0, newPosition, 0);
+                
+            MoveNumber(fromArray, fromIndex, toArray, toIndex);
+                
+            currentDisc = null;
+            isDiscUp = false;
+
+            if (currentPeg != 3) return;
+
+            for (int i = 0; i < peg3.Length; i++)
+            {
+                if (peg3[i] == 0)
+                {
+                    return;
+                }
+            }
+            
+            winInfo.gameObject.SetActive(true);
+        }
+    }
+
+    // Couldn't implement yet ):
+    public void RestartGame()
+    {
+        winInfo.gameObject.SetActive(false);
     }
 
     private void UpdatePointer()
@@ -110,5 +194,73 @@ public class NewHanoiTower : MonoBehaviour
             default:
                 return peg1Transform;
         }
+    }
+
+    private Transform PopDiscFromCurrentPeg()
+    {
+        Transform currentPegTransform = GetPegTransform(currentPeg);
+
+        for (int i = currentPegTransform.childCount - 1; i >= 0; i--)
+        {
+            Transform disc = currentPegTransform.GetChild(i);
+            
+            if (disc.CompareTag("Disc")) return disc;
+        }
+        
+        return null;
+    }
+
+    private void MoveNumber(int[] fromArray, int fromIndex, int[] toArray, int toIndex)
+    {
+        int value = fromArray[fromIndex];
+
+        fromArray[fromIndex] = 0;
+        toArray[toIndex] = value;
+    }
+    
+    private int[] GetPeg(int pegNumber)
+    {
+        switch (pegNumber)
+        {
+            case 1:
+                return peg1;
+            case 2 :
+                return peg2;
+            case 3:
+                return peg3;
+            default:
+                return peg1;
+        }
+    }
+    
+    private int GetTopNumberIndex(int[] peg)
+    {
+        for (int i = 0; i < peg.Length; i++)
+        {
+            if (peg[i] != 0) return i;
+        }
+        
+        return -1;
+    }
+
+    private int GetIndexOfFreeSlot(int[] peg)
+    {
+        for (int i = peg.Length - 1; i >= 0; i--)
+        {
+            if (peg[i] == 0) return i;
+        }
+        
+        return -1;
+    }
+
+    private bool CanAddToPeg(int value, int[] peg)
+    {
+        int topNumberIndex = GetTopNumberIndex(peg);
+
+        if (topNumberIndex == -1) return true;
+
+        int topNumber = peg[topNumberIndex];
+
+        return topNumber > value;
     }
 }
